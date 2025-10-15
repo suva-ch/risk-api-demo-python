78b9ea1ad175520c1c0e464eb16e6a5facb88225
Leif Jakob <leif.jakob@suva.ch>
2025-10-15 07:46:36 +0200

    more demo code - get token with scopes

--- demoWithStubs.py
+++ demoWithStubs.py
@@ -18,7 +18,7 @@ import jwt  # PyJWT
 import time
 import uuid
 import json
-from typing import Dict, List, Optional, Any
+from typing import Dict, List, Optional, Any, Callable
 import uuid
 import pprint
 import argparse
@@ -35,6 +35,7 @@ def getAccessToken() -> Optional[str]:
     # Token request details
     token_data = {
         'grant_type': 'client_credentials',
+        'scope': 'openid',
         'client_id': config['Credentials']['ClientID'],
         'client_secret': config['Credentials']['ClientSecret'],
     }
@@ -74,6 +75,7 @@ def getAccessTokenJWT() -> Optional[str]:
     # Token request details
     token_data = {
         'grant_type': 'client_credentials',
+        'scope': 'openid',
         'client_assertion_type': 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
         'client_assertion': encoded_jwt
     }
@@ -118,14 +120,32 @@ def main() -> None:
     }
 
     def dumpUserinfo() -> None:
-        print(api_config.access_token)
-        response = requests.get(config['Credentials']['UserInfoURL'],
+
+        token = jwt.decode(str(api_config.access_token), options={"verify_signature": False})
+        log.info('token=%s', token)
+
+        # Extract first number from orgs[0]['sapId']
+        sap_id = None
+        orgs = token.get('orgs', [])
+        if orgs and isinstance(orgs, list) and 'sapId' in orgs[0]:
+            sap_id_str = orgs[0]['sapId']
+            # Extract first number from sapId string
+            import re
+            match = re.search(r'\d+', sap_id_str)
+            if match:
+                sap_id = match.group(0)
+
+        log.info('customer ID:%s', sap_id)
+
+        response = requests.get(f'https://intg.suva.ch/app/eservices/partner/svc/v3/geschaeftspartner/' + sap_id,
                             headers={
                                 'User-Agent': config['Client']['User-Agent'],
+                                'Content-Type': 'application/json',
                                 'Authorization': 'Bearer ' + str(api_config.access_token)
                                 }
                             )
-        print(response.json())
+        customer = response.json()
+        log.info('customer is:%s - %s', customer['name1'], customer['uidNrFormatiert'])
 
     def demoListaSuvaOccupationCodes() -> None:
         from openapi_client.api.suva_occupation_codes_api import SuvaOccupationCodesApi
@@ -172,7 +192,13 @@ def main() -> None:
 
             #print(pprint.pformat(object=json.loads(c.model_dump_json()), indent=1, width=200))
 
+    def demoListAllEvents()->None:
+        _demoEvents(clear=False)
+
     def demoClearAllEvents()->None:
+        _demoEvents(clear=True)
+
+    def _demoEvents(clear : bool)->None:
         from openapi_client.api.events_api import EventsApi
         from openapi_client.models.event import Event
         from openapi_client.models.event_occupation_code_submitted import EventOccupationCodeSubmitted
@@ -189,10 +215,10 @@ def main() -> None:
                 if isinstance(e.detail.actual_instance, EventOccupationCodeSubmitted):
                     d : EventOccupationCodeSubmitted = e.detail.actual_instance
                     log.info('EventOccupationCodeSubmitted=%s', d)
-            if e.id is not None:
+            if e.id is not None and clear:
                 api_ev.acknowledge_event(**x_headers, event_id=e.id)
 
-    def demoActivateSuvaCode() -> None:
+    def demoActivateSuvaCode(requested_suva_id : str) -> None:
         from openapi_client.api.suva_occupation_codes_api import SuvaOccupationCodesApi
         from openapi_client.api.occupation_codes_api import OccupationCodesApi
         from openapi_client.models.suva_occupation_code import SuvaOccupationCode
@@ -202,23 +228,126 @@ def main() -> None:
         api_list = SuvaOccupationCodesApi(api_client=client)
         res_list: List[SuvaOccupationCode] = api_list.get_suva_occupation_codes(**x_headers)
         log.info('received %i codes', len(res_list))
-        code = res_list[random.randint(0, len(res_list)-1)]
-        log.info('code=%s description=%s', code.id, code.descriptions[0].model_dump_json())
+
+        code : Optional[SuvaOccupationCode] = None
+
+        if requested_suva_id is None:
+            code = res_list[random.randint(0, len(res_list)-1)]
+            log.info('code=%s description=%s', code.id, code.descriptions[0].model_dump_json())
+        else:
+            for c in res_list:
+                if c.id == requested_suva_id:
+                    code = c
+                    break
+
+        if code is None:
+            raise Exception('no code')
 
         api_oc = OccupationCodesApi(api_client=client)
         activate_code = OccupationCodeActivation(suvaOccupationCodeId=code.id)
+        activate_code.occupation_code_number1 = str(uuid.uuid4())
+        activate_code.occupation_code_number2 = 'debug Activate'
+
         res_activate = api_oc.activate_occupation_code(occupation_code_activation=activate_code, **x_headers)
         print(pprint.pformat(object=json.loads(res_activate.model_dump_json()), indent=1, width=200))
 
-    def demoInActivateCode() -> None:
+    def demoInAtivateSuvaCode(requested_suva_id : str) -> None:
+
+        if requested_suva_id is None:
+            raise Exception('requested_suva_id needed')
+
+        from openapi_client.api.suva_occupation_codes_api import SuvaOccupationCodesApi
+        from openapi_client.api.occupation_codes_api import OccupationCodesApi
+        from openapi_client.models.suva_occupation_code import SuvaOccupationCode
+        from openapi_client.models.occupation_code_activation import OccupationCodeActivation
+        from openapi_client.models.occupation_code import OccupationCode
+
+        from openapi_client.api.occupation_codes_api import OccupationCodesApi
+        from openapi_client.models.occupation_code import OccupationCode
+
+        api_oc = OccupationCodesApi(api_client=client)
+        codes : List[OccupationCode] = api_oc.get_occupation_codes(**x_headers)
+
+        code : Optional[SuvaOccupationCode] = None
+        for c in codes:
+                if c.suva_occupation_code.id == requested_suva_id:
+                    code = c
+                    break
+
+        if code is None:
+            raise Exception('no code')
+
+        api_oc = OccupationCodesApi(api_client=client)
+        res = api_oc.deactivate_occupation_code(**x_headers, occupation_code_id=c.id)
+        print('success')
+
+    def demoInActivateCode(occupation_code_number1: Optional[str] = None) -> None:
         from openapi_client.api.occupation_codes_api import OccupationCodesApi
         from openapi_client.models.occupation_code import OccupationCode
 
         api_oc = OccupationCodesApi(api_client=client)
 
         codes : List[OccupationCode] = api_oc.get_occupation_codes(**x_headers)
-        for c in codes:
-            print(pprint.pformat(object=json.loads(c.model_dump_json()), indent=1, width=200))
+        if occupation_code_number1:
+            # Try to find and inactivate the code
+            found = False
+            for c in codes:
+                if c.occupation_code_number1 == occupation_code_number1:
+                    if not c.active:
+                        raise Exception(f"Code with occupation_code_number1={occupation_code_number1} is already inactive")
+
+                    print(f"Inactivating code with occupation_code_number1={occupation_code_number1}")
+                    found = True
+
+                    res = api_oc.deactivate_occupation_code(**x_headers, occupation_code_id=c.id)
+
+                    print('success')
+
+                    break
+            if not found:
+                print(f"No code found with occupation_code_number1={occupation_code_number1}")
+        else:
+            for c in codes:
+                print(pprint.pformat(object=json.loads(c.model_dump_json()), indent=1, width=200))
+
+    def demoReactivateCode(occupation_code_number1: Optional[str] = None) -> None:
+        from openapi_client.api.occupation_codes_api import OccupationCodesApi
+        from openapi_client.models.occupation_code import OccupationCode
+        from openapi_client.models.update_occupation_code import UpdateOccupationCode
+
+        api_oc = OccupationCodesApi(api_client=client)
+
+        codes : List[OccupationCode] = api_oc.get_occupation_codes(**x_headers)
+        if occupation_code_number1:
+            found = False
+            for c in codes:
+                if c.occupation_code_number1 == occupation_code_number1:
+                    if c.active:
+                        raise Exception(f"Code with occupation_code_number1={occupation_code_number1} is already active")
+                    
+                    print(f"Reactivating code with occupation_code_number1={occupation_code_number1}")
+                    found = True
+
+                    if c.occupation_code_number1 is None:
+                        print('Suva standard code')
+                    else:
+                        print('individual code')
+
+                        upd : UpdateOccupationCode = UpdateOccupationCode(
+                            occupationCodeNumber1=c.occupation_code_number1,
+                            occupationCodeNumber2=str(c.occupation_code_number2),
+                        )
+                        api_oc.update_occupation_code(**x_headers, occupation_code_id=c.id, update_occupation_code=upd)
+
+                    print('success')
+
+                    break
+            if not found:
+                print(f"No code found with occupation_code_number1={occupation_code_number1}")
+        else:
+            for c in codes:
+                print(pprint.pformat(object=json.loads(c.model_dump_json()), indent=1, width=200))
+
 
     def demoSubmitOccupationCode() -> None:
         from openapi_client.api.occupation_codes_api import OccupationCodesApi
@@ -235,14 +364,20 @@ def main() -> None:
         api_ev = EventsApi(api_client=client)
 
         # submit new event
-        desc = OccupationDescription(language=Language.DE, gender=Gender.MALE, value='Lebkuchenhausbauer') 
+        descs : List[OccupationDescription] = []
         
-        code_uuid = str(uuid.uuid5(namespace=MY_UUID_NAMESPACE, name=desc.value)) # generate UUID from string
+        desc = OccupationDescription(language=Language.DE, gender=Gender.MALE, value='Lebkuchenhausbauer')
+
+        #descs.append(OccupationDescription(language=Language.DE, gender=Gender.GENDERLESS, value='Gartenzwergtöpfer*in (m/w/d)'))
+        #descs.append(OccupationDescription(language=Language.DE, gender=Gender.MALE, value='Gartenzwergtöpfer'))
+        #descs.append(OccupationDescription(language=Language.DE, gender=Gender.FEMALE, value='Gartenzwergtöpferin'))
+
+        code_uuid = str(uuid.uuid5(namespace=MY_UUID_NAMESPACE, name=descs[0].value)) # generate UUID from string
 
         code = SubmitOccupationCode(occupationCodeNumber1=code_uuid, 
                                     occupationCodeNumber2=code_uuid, 
                                     preferredLanguage=Language.DE, 
-                                    descriptions=[desc])
+                                    descriptions=descs)
         try:
             res = api_oc.submit_occupation_code(**x_headers, submit_occupation_code=code)
             print(pprint.pformat(object=json.loads(res.model_dump_json()), indent=1, width=200))
@@ -273,13 +408,16 @@ def main() -> None:
 
         log.info('response=%s', response.occupation)
 
-    demo_commands = {
+    demo_commands : Dict[str, Callable[..., Any]] = {
         'DumpUserinfo': dumpUserinfo,
         'ListaSuvaOccupationCodes': demoListaSuvaOccupationCodes,
         'ListaMyOccupationCodes': demoListaMyOccupationCodes,
         'ListCompanyPartProfile': demoListCompanyPartProfile, # not implemented 2025-05-14
         'ActivateSuvaCode': demoActivateSuvaCode,
+        'InActivateSuvaCode': demoInAtivateSuvaCode,
         'InActivateCode': demoInActivateCode,
+        'ReActivateCode': demoReactivateCode,
+        'ListAllEvents': demoListAllEvents,
         'ClearAllEvents': demoClearAllEvents,
         'SubmitOccupationCode': demoSubmitOccupationCode,
     }
@@ -287,7 +425,7 @@ def main() -> None:
     parser = argparse.ArgumentParser(description='call demo')
     parser.add_argument('--debug', action='store_true', default=False, help='enable debug output')
     parser.add_argument('demo', choices=demo_commands.keys(), help='chose demo')
-   
+    parser.add_argument('param', nargs='?', default=None, help='optional parameter for demo function')
     args = parser.parse_args()
 
     if args.debug:
@@ -310,8 +448,13 @@ def main() -> None:
     client.default_headers['Authorization'] = 'Bearer ' + api_config.access_token  # see https://github.com/OpenAPITools/openapi-generator/issues/18041
 
     # call demo
-    demo_func = demo_commands[args.demo]
-    demo_func()
+    demo_func : Callable[..., Any] = demo_commands[args.demo]
+
+    import inspect
+    if len(inspect.signature(demo_func).parameters) > 0:
+        demo_func(args.param)
+    else:
+        demo_func()
 
 if __name__ == '__main__':
     main()
